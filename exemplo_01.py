28b41ac039637c892e714460dbd54719261a885f
lvgalvaofilho <lvgalvaofilho@gmail.com>
2024-03-19 13:39:12 -0300

    update

---
+++ exemplo_01.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+
+# Conectar ao SQLite em memória
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+
+print("Conexão com SQLite estabelecida.")
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+# Criar as tabelas no banco de dados
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with Session() as session:
+    novo_usuario = Usuario(nome='Ana', idade=25)
+    session.add(novo_usuario)
